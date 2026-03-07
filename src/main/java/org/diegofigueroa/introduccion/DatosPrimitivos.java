3ccde16c574c58f9a1d28740dc08ad20614a9454
DiegoFigueSev <diego.figueroa.sevillano.dev@outlook.com>
2026-03-07 10:34:40 -0400

    introduction/operators and variables

---
+++ src/main/java/org/diegofigueroa/introduccion/DatosPrimitivos.java
@@ -0,0 +1,41 @@
+package org.diegofigueroa.introduccion;
+
+public class DatosPrimitivos {
+
+    /**
+     * Que son los datos primitivos?
+     *
+     * Son tipos de datos que contienen un solo valor
+     * Si yo declaro varaibles enteras SOLO ALMACENO ENTEROS
+     */
+
+    /**
+     * Tipos de datos:
+     * byte - Numero pequenos
+     * short - Numeros pequenos
+     * int = numeros enteros generales
+     * long = numeros enteros muy grandes
+     * float = Numeros decimales con precicion simple
+     * double = Numeros decimales con precision doble
+     * char = Almacena caracteres individuales
+     * boolean = Valores logicos (Verdaderos/Falso)
+     */
+
+    // Tipo de dato: Boolean
+    boolean isLoading = false;
+    boolean isActive = true;
+
+    // Tipo de dato: Char
+    char aCharacter = 'a';
+    char oneCharacter = '1';
+
+    // Tipo de dato numerico
+    byte enteroByte = 127;
+    short enteroShort = 32767;
+    int enteroInt = 21312312;
+    long enteroLong = 12312312311231231L;
+
+    // Tipo de dato con punto flotante
+    float valorFloat = 3.412412f;
+    double valorDouble = 123.23;
+}
---
+++ src/main/java/org/diegofigueroa/introduccion/introduccion.md
@@ -0,0 +1,9 @@
+# Introduccion al lenguaje
+
+Que vamos a ver en este modulo?
+
+Dentro de este modulo vamos a ver temas como:
+* Variables y constantes 
+* Tipos de datos 
+* Operadores basicos
+* Etc
