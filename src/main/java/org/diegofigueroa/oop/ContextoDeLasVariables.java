263873bc8e3c632567fa07fc0ad40d2feb0c87c3
DiegoFigueSev <diego.figueroa.sevillano.dev@outlook.com>
2026-03-09 12:24:56 -0400

    clases wrapper

---
+++ src/main/java/org/diegofigueroa/oop/ContextoDeLasVariables.java
@@ -0,0 +1,17 @@
+package org.diegofigueroa.oop;
+
+public class ContextoDeLasVariables {
+    {
+        // * Esta variable solo vive en mi metodo dentor de mis llaves
+        int number = 10;
+
+        // * Variables locales; Solo viven dentro de las llaves, dentro de la funcion, metodo, clase
+        int myVar = 24;
+
+        // * Las cariables de clase son las que se definen dentro de una clase, son los atributos de la clase
+
+        // * Las variables locales, existen unicamente durante la ejecucion del metodo donde se declaran
+        // * Las variables de instancia existen mientras la instancia de la clase esten en memoria
+        // * Las variables de clase existen siempre
+    }
+}
