5508d5057a635875d5c7868b0933ef16b0218feb
DiegoFigueSev <diego.figueroa.sevillano.dev@outlook.com>
2026-03-08 21:21:20 -0400

    atirbutos y metodos

---
+++ src/main/java/org/diegofigueroa/oop/AtributosYMetodos.java
@@ -0,0 +1,40 @@
+package org.diegofigueroa.oop;
+
+public class AtributosYMetodos {
+
+    {
+
+        // * Instanciamos la clase al crear un objeto con esa clase o molde
+        House house = new House();
+        house.size = 32;
+
+        House house2 = new House();
+        house2.size = 32;
+
+        house.openDoor();
+
+        System.out.println(house2.calculateArea());
+
+    }
+
+}
+
+// * CREACION DEL MOLDE/CLASE HOUSE
+class House {
+    // ? ATRIBUTOS
+    int doors;
+    int windows;
+    double size;
+
+    // * Nos permite crear muchos objetos del mismo tipo pero con diferentes caracteristicas
+
+    // ? METODOS
+
+    void openDoor(){
+        System.out.println("La puerta esta abierta");
+    }
+
+    double calculateArea(){
+        return size * size;
+    }
+}
---
+++ src/main/java/org/diegofigueroa/oop/ModificadoresDeAcceso.java
@@ -0,0 +1,59 @@
+package org.diegofigueroa.oop;
+
+public class ModificadoresDeAcceso {
+
+    {
+
+        Pet myPet = new Pet("Guardian", 32, false);
+
+        String name = myPet.getName();
+    }
+
+}
+
+
+class Pet {
+    /**
+     * Los modificadores de acceso estan relacionados con el ENCAPSULAMIENTO
+     *
+     * Este nos ayuda a controlar el acceso al os atributos
+     */
+
+    public String name; // ? Es visible en todos los lugares
+    protected int age; // ? protected: es visible en el paquete y clases q lo heredan
+    private boolean isCat; // ? Los atributos privados son solo visibles dentro de la clase
+
+    public Pet(String name, int age, boolean isCat) {
+        this.name = name;
+        this.age = age;
+        this.isCat = isCat;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isCat() {
+        return isCat;
+    }
+
+    public void setCat(boolean cat) {
+        isCat = cat;
+    }
+
+    public void miau(){
+        System.out.println("Miau");
+    }
+}
